f224e35d613e0b6e1eaa7595bd1275e8b4baed07
shineunjeong <dkfah1229@naver.com>
2022-04-25 15:11:06 +0900

    두번째

--- test/src/main/java/com/user/UserDTO.java
+++ test/src/main/java/com/user/UserDTO.java
@@ -2,7 +2,8 @@ package com.user;
 
 public class UserDTO {
 	private String userId;
-
+	private String userName;
+	
 	public String getUserId() {
 		return userId;
 	}
@@ -10,6 +11,12 @@ public class UserDTO {
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
-	
-	
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 }
