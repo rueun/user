c4957f7d08478a5b54b5d6495f2c2703bb81ada5
shineunjeong <dkfah1229@naver.com>
2022-04-25 15:05:19 +0900

    첫번째 테스트

---
+++ test/src/main/java/com/user/UserDTO.java
@@ -0,0 +1,15 @@
+package com.user;
+
+public class UserDTO {
+	private String userId;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
+}
